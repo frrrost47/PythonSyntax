fed8daae1ff8bb9d1b3d08c47af270426b0cc6b0
DESKTOP-CUA7CG0\depos <depositvs@yandex.ru>
2022-07-04 23:06:51 +0700

    Befor err

---
+++ 04 More Control Flow Tools/470 Functions.py
@@ -0,0 +1,32 @@
+def fib(n):    # write Fibonacci series up to n
+    a, b = 0, 1
+    while a < n:
+        print(a, end=' ')
+        a, b = b, a+b
+    print()
+
+# fib(100) # 0 1 1 2 3 5 8 13 21 34 55 89
+# f = fib(10) # 0 1 1 2 3 5 8
+
+
+def fib2(n):
+    result = []
+    a, b = 0, 1
+    while a < n:
+        result.append(a)
+        a, b = b, a+b
+    return result
+
+print(fib2(20))
+
+def ask_ok(prompt, retries=4, reminder='Please try again!'):
+    while True:
+        ok = input(prompt)
+        if ok in ('y', 'ye', 'yes'):
+            return True
+        if ok in ('n', 'no', 'nop', 'nope'):
+            return False
+        retries = retries - 1
+        if retries < 0:
+            raise ValueError('invalid user response')
+        print(reminder)
