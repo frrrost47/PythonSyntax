7435bc2a9f2f833518af89c5ce1457cd28894a28
DESKTOP-CUA7CG0\depos <depositvs@yandex.ru>
2022-07-05 00:46:16 +0700

    Minor

--- 04 More Control Flow Tools/470 Functions.py
+++ 04 More Control Flow Tools/470 Functions.py
@@ -17,7 +17,7 @@ def fib2(n):
         a, b = b, a+b
     return result
 
-print(fib2(20))
+#print(fib2(20))
 
 def ask_ok(prompt, retries=4, reminder='Please try again!'):
     while True:
@@ -30,3 +30,42 @@ def ask_ok(prompt, retries=4, reminder='Please try again!'):
         if retries < 0:
             raise ValueError('invalid user response')
         print(reminder)
+
+i = 5
+
+def f(arg=i):
+    print(arg)
+
+i = 6
+# f() # 5
+
+# непривычно работает память:
+
+def f1(a, L=[]):
+    L.append(a)
+    return L
+
+def f2(a, L=[]):
+    L.append(a)
+    return L
+
+print(f(1), f(2), f(3), f2(4), f2(5), f2(6))
+
+
+list = [[[1], 2], [3]]
+
+array2 = list[0]
+
+array2[0][0] = 99
+print(list)
+
+# * = list, **=array
+def cheeseshop(kind, *arguments, **keywords):
+    print("kind = ", kind)
+    for arg in arguments:
+        print(arg)
+    print("-" * 40)
+    for key, value in keywords.copy().items():
+        print(key, ":", value)
+
+cheeseshop("Test", "1", "2", aaa="3", ooo="4", eee="5")
